1c2f20bf06b33e2e8d4470b9bd7fe2644520d37f
Saif Kantrikar <skantrik@cisco.com>
2019-04-16 23:24:22 -0700

    Initial commit with patterns and matcher

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const Readline = require('readline');
+const rl = Readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+
+const matcher = require('./matcher');
+
+rl.setPrompt('> ');
+rl.prompt();
+rl.on('line', reply => {
+    //console.log(`You said :: ${reply}`);
+    matcher(reply, data => {
+        switch(data.intent) {
+            case 'Hello':
+                console.log("A Big Hello from SimpleBot");
+                rl.prompt();
+                break;
+            default: {
+                console.log("I am not sure what you mean. Can you repeat that?");
+                rl.prompt();
+            }
+        }
+    });
+})
---
+++ patterns/index.js
@@ -0,0 +1,9 @@
+const patternDict = [{
+    pattern: '\\b(Hi|Hello|Hey)\\b',
+    intent: 'Hello'
+}, {
+    pattern: '\\b(bye|exit)\\b',
+    intent: 'Exit'
+}];
+
+module.exports = patternDict;
